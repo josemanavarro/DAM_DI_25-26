c7b25a4d61b644dc9a3c4ea74cb2c33bd41e462b
josemanavarro <josemanuel.navarro@iesalandalus.org>
2025-10-01 09:34:43 +0200

    Subida del primer ejemplo de clase

---
+++ ejemploClase1/src/main/java/di/dam/ejemploclase1/EjemploClase1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package di.dam.ejemploclase1;
+
+import di.dam.ejemploclase1.vista.JFramePrincipal;
+
+/**
+ *
+ * @author jose
+ */
+public class EjemploClase1 {
+
+    public static void main(String[] args) {
+        JFramePrincipal jfp = new JFramePrincipal();
+        jfp.setVisible(true);
+    }
+}
---
+++ ejemploClase1/src/main/java/di/dam/ejemploclase1/datos/Estudiante.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package di.dam.ejemploclase1.datos;
+
+/**
+ *
+ * @author jose
+ */
+public class Estudiante {
+    private String nombre;
+    private String apellidos;
+    private String curso;
+
+    public Estudiante(String nombre, String apellidos, String curso) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.curso = curso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    
+    
+}
