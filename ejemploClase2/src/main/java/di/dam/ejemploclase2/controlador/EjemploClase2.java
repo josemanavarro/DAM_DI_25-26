7f2722027b5fabf3c6a012de69b0ed3a1b55292e
josemanavarro <josemanuel.navarro@iesalandalus.org>
2025-10-06 19:28:49 +0200

    Subo el segundo ejemplo de clase

---
+++ ejemploClase2/src/main/java/di/dam/ejemploclase2/controlador/EjemploClase2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package di.dam.ejemploclase2.controlador;
+
+import di.dam.ejemploclase2.vista.JFramePrincipal;
+
+/**
+ *
+ * @author jose
+ */
+public class EjemploClase2 {
+
+    public static void main(String[] args) {
+        
+        // Definimos y mostramos la ventana principal
+        JFramePrincipal jfp = new JFramePrincipal();
+        jfp.setVisible(true);
+    }
+}
